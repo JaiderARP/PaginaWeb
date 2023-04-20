50a407f221f1a63c19fdb81f67f83ccc36aa0f20
JaiderARP <mateo08112@gmail.com>
2023-04-19 21:27:59 -0500

    modi

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,97 +1,159 @@
-let imgBox = document.getElementById("imgBox");
+let idImgMain1 = document.getElementById("idImgMain1");
+let idImgMain2 = document.getElementById("idImgMain2");
+let idImgMain3 = document.getElementById("idImgMain3");
+let idImgMain4 = document.getElementById("idImgMain4");
 let imgMini1 = document.getElementById("imgMini1");
 let imgMini2 = document.getElementById("imgMini2");
 let imgMini3 = document.getElementById("imgMini3");
 let imgMini4 = document.getElementById("imgMini4");
+let idFocus1 = document.getElementById("idFocus1");
+let idFocus2 = document.getElementById("idFocus2");
+let idFocus3 = document.getElementById("idFocus3");
+let idFocus4 = document.getElementById("idFocus4");
+let idImgMain = document.getElementById("idImgMain");
+let imgPredeterminada = document.getElementById("imgPredeterminada");
+let idMinus = document.getElementById("idMinus");
+let idPlus = document.getElementById("idPlus");
+let idPrecioTotal = document.getElementById("idPrecioTotal");
+let idcantidadNumero = document.getElementById("idcantidadNumero");
 
 
-/*  ------- BOTONES CAMBIO DE IMG ------- */
-
-// imgMini1.addEventListener("click", function(){
-//     imgBox.setAttribute("src","img/sneakers/producto1.png")
-//     boxImgMini1.classList.add("box-img-mini")
-//     boxImgMini2.classList.remove("box-img-mini")
-//     boxImgMini3.classList.remove("box-img-mini")
-//     boxImgMini4.classList.remove("box-img-mini")
-//     boxImgMini1.classList.add("img-mini-act")
-//     boxImgMini2.classList.remove("img-mini-act")
-//     boxImgMini3.classList.remove("img-mini-act")
-//     boxImgMini4.classList.remove("img-mini-act")
-// });
-// imgMini2.addEventListener("click", function(){
-//     imgBox.setAttribute("src","img/sneakers/image-product-2.png")
-//     boxImgMini1.classList.remove("box-img-mini")
-//     boxImgMini2.classList.add("box-img-mini")
-//     boxImgMini3.classList.remove("box-img-mini")
-//     boxImgMini4.classList.remove("box-img-mini")
-//     boxImgMini1.classList.remove("img-mini-act")
-//     boxImgMini2.classList.add("img-mini-act")
-//     boxImgMini3.classList.remove("img-mini-act")
-//     boxImgMini4.classList.remove("img-mini-act")
-// });
-// imgMini3.addEventListener("click", function(){
-//     imgBox.setAttribute("src","img/sneakers/image-product-3.png")
-//     boxImgMini1.classList.remove("box-img-mini")
-//     boxImgMini2.classList.remove("box-img-mini")
-//     boxImgMini3.classList.add("box-img-mini")
-//     boxImgMini4.classList.remove("box-img-mini")
-//     boxImgMini1.classList.remove("img-mini-act")
-//     boxImgMini2.classList.remove("img-mini-act")
-//     boxImgMini3.classList.add("img-mini-act")
-//     boxImgMini4.classList.remove("img-mini-act")
-// });
-// imgMini4.addEventListener("click", function(){
-//     imgBox.setAttribute("src","img/sneakers/image-product-4.png")
-//     boxImgMini1.classList.remove("box-img-mini")
-//     boxImgMini2.classList.remove("box-img-mini")
-//     boxImgMini3.classList.remove("box-img-mini")
-//     boxImgMini4.classList.add("box-img-mini")
-//     boxImgMini1.classList.remove("img-mini-act")
-//     boxImgMini2.classList.remove("img-mini-act")
-//     boxImgMini3.classList.remove("img-mini-act")
-//     boxImgMini4.classList.add("img-mini-act")
-// });
-
-function cambiarImagen(nombreImagen, mini) {
-    imgBox.setAttribute("src", `img/sneakers/${nombreImagen}.png`);
-    imgMinis.forEach(imgMini => imgMini.classList.remove("box-img-mini", "img-mini-act"));
-    mini.classList.add("box-img-mini", "img-mini-act");
-}
-
-const imgMinis = document.querySelectorAll(".img-mini");
-imgMinis.forEach((imgMini, i) => imgMini.addEventListener("click", () => cambiarImagen(`producto${i + 1}`, imgMini)));
-
-
-imgMini1.addEventListener("click", function(){
-    cambiarImagen("producto1", imgMini1);
-    imgBox.classList.add("rotate")
-});
-
-imgMini2.addEventListener("click", function(){
-    cambiarImagen("image-product-2", imgMini2);
-    imgBox.classList.add("rotate")
-});
-
-imgMini3.addEventListener("click", function(){
-    cambiarImagen("image-product-3", imgMini3);
-    imgBox.classList.add("rotate")
-});
-
-imgMini4.addEventListener("click", function(){
-    cambiarImagen("image-product-4", imgMini4);
-    imgBox.classList.add("rotate")
-});
-
-/* -------- BOTONES DE COMPRA ------ */
-
-let btnRestar = document.getElementById("btnRestar");
-let btnSumar = document.getElementById("btnSumar");
-
-btnRestar.addEventListener("click",function(){
-    alert("click")
-});
-btnSumar.addEventListener("click",function(){
-    alert("otro click")
+imgMini1.addEventListener("click", function () {
+    imgPredeterminada.classList.add("img-box-invisible")
+    /*     idImgMain.setAttribute("src", "img/imgCarrito/image-product-1.jpg") */
+    /*     if (imgMini1.classList.contains("img-opacity")) {
+            imgMini1.classList.remove("img-opacity");
+            idFocus1.classList.remove("focus")
+            idImgMain1.classList.remove("img-main-animation1")
+        } else {  } */
+    idImgMain1.classList.add("img-main-animation1")
+    idImgMain2.classList.remove("img-main-animation2")
+    idImgMain3.classList.remove("img-main-animation3")
+    idImgMain4.classList.remove("img-main-animation4")
+    idImgMain1.classList.remove("img-box-invisible")
+    idImgMain2.classList.add("img-box-invisible")
+    idImgMain3.classList.add("img-box-invisible")
+    idImgMain4.classList.add("img-box-invisible")
+    imgMini1.classList.add("img-opacity");
+    idFocus1.classList.add("focus")
+    idFocus2.classList.remove("focus")
+    idFocus3.classList.remove("focus")
+    idFocus4.classList.remove("focus")
+    imgMini2.classList.remove("img-opacity");
+    imgMini3.classList.remove("img-opacity");
+    imgMini4.classList.remove("img-opacity");
+})
+imgMini2.addEventListener("click", function () {
+    imgPredeterminada.classList.add("img-box-invisible")
+    /* idImgMain.setAttribute("src", "img/imgCarrito/image-product-2.jpg") */
+    /*     if (imgMini2.classList.contains("img-opacity")) {
+            imgMini2.classList.remove("img-opacity");
+            idFocus2.classList.remove("focus")
+            idImgMain2.classList.remove("img-main-animation2")
+        } else {} */
+    idImgMain2.classList.add("img-main-animation2")
+    idImgMain3.classList.remove("img-main-animation3")
+    idImgMain4.classList.remove("img-main-animation4")
+    idImgMain1.classList.remove("img-main-animation1")
+    idImgMain2.classList.remove("img-box-invisible")
+    idImgMain1.classList.add("img-box-invisible")
+    idImgMain3.classList.add("img-box-invisible")
+    idImgMain4.classList.add("img-box-invisible")
+    imgMini2.classList.add("img-opacity");
+    idFocus2.classList.add("focus")
+    idFocus1.classList.remove("focus")
+    idFocus3.classList.remove("focus")
+    idFocus4.classList.remove("focus")
+    imgMini1.classList.remove("img-opacity");
+    imgMini3.classList.remove("img-opacity");
+    imgMini4.classList.remove("img-opacity");
+})
+imgMini3.addEventListener("click", function () {
+    imgPredeterminada.classList.add("img-box-invisible")
+    /* idImgMain.setAttribute("src", "img/imgCarrito/image-product-3.jpg") */
+    /*     if (imgMini3.classList.contains("img-opacity")) {
+            imgMini3.classList.remove("img-opacity");
+            idFocus3.classList.remove("focus")
+            idImgMain3.classList.remove("img-main-animation3")
+        } else {  } */
+    idImgMain3.classList.add("img-main-animation3")
+    idImgMain2.classList.remove("img-main-animation2")
+    idImgMain1.classList.remove("img-main-animation1")
+    idImgMain4.classList.remove("img-main-animation4")
+    idImgMain3.classList.remove("img-box-invisible")
+    idImgMain2.classList.add("img-box-invisible")
+    idImgMain1.classList.add("img-box-invisible")
+    idImgMain4.classList.add("img-box-invisible")
+    imgMini3.classList.add("img-opacity");
+    idFocus3.classList.add("focus")
+    idFocus2.classList.remove("focus")
+    idFocus1.classList.remove("focus")
+    idFocus4.classList.remove("focus")
+    imgMini2.classList.remove("img-opacity");
+    imgMini1.classList.remove("img-opacity");
+    imgMini4.classList.remove("img-opacity");
+})
+imgMini4.addEventListener("click", function () {
+    imgPredeterminada.classList.add("img-box-invisible")
+    /* idImgMain.setAttribute("src", "img/imgCarrito/image-product-4.jpg") */
+    /*     if (imgMini4.classList.contains("img-opacity")) {
+            imgMini4.classList.remove("img-opacity");
+            idFocus4.classList.remove("focus")
+            idImgMain4.classList.remove("img-main-animation4")
+        } else {  } */
+    idImgMain4.classList.add("img-main-animation4")
+    idImgMain2.classList.remove("img-main-animation2")
+    idImgMain3.classList.remove("img-main-animation3")
+    idImgMain1.classList.remove("img-main-animation1")
+    idImgMain4.classList.remove("img-box-invisible")
+    idImgMain2.classList.add("img-box-invisible")
+    idImgMain1.classList.add("img-box-invisible")
+    idImgMain3.classList.add("img-box-invisible")
+    imgMini4.classList.add("img-opacity");
+    idFocus4.classList.add("focus")
+    idFocus2.classList.remove("focus")
+    idFocus3.classList.remove("focus")
+    idFocus1.classList.remove("focus")
+    imgMini2.classList.remove("img-opacity");
+    imgMini3.classList.remove("img-opacity");
+    imgMini1.classList.remove("img-opacity");
 })
 
+idMinus.addEventListener("click", function () {
+    /*     let nodeToAdd = document.createElement("p");
+        let nodeText = document.createTextNode("Holis");
+        nodeToAdd.appendChild(nodeText);
+        idCantidadNumero.appendChild(nodeToAdd); */
+    let idcantidadNumeroProducto = document.getElementById("idcantidadNumeroProducto").innerHTML;
+    let cantidadNumeroInt = parseInt(idcantidadNumeroProducto);
+    let totalCantidadNumeroInt = cantidadNumeroInt + -1;
+    let totalCantidadNumeroTxt = totalCantidadNumeroInt.toString();
+    document.getElementById("idcantidadNumeroProducto").innerHTML = totalCantidadNumeroTxt;
+    let precioZapatos = 250000;
+    let descuento = precioZapatos * 0.5;
+    let precioTotal = (precioZapatos - descuento) * totalCantidadNumeroInt;
+    if (cantidadNumeroInt == 0) {
+        document.getElementById("idcantidadNumeroProducto").innerHTML = 0;
+        precioTotal = (precioZapatos - descuento) * 0;
+        console.log(precioTotal)
+    }
+    idPrecioTotal.innerHTML = "$" + precioTotal.toLocaleString('mx') ;
 
+})
+idPlus.addEventListener("click", function () {
+    let idcantidadNumeroProducto = document.getElementById("idcantidadNumeroProducto").innerHTML;
+    let cantidadNumeroInt = parseInt(idcantidadNumeroProducto);
+    let totalCantidadNumeroInt = cantidadNumeroInt + 1;
+    let totalCantidadNumeroTxt = totalCantidadNumeroInt.toString();
+    document.getElementById("idcantidadNumeroProducto").innerHTML = totalCantidadNumeroTxt;
+    console.log(totalCantidadNumeroTxt)
+    let precioZapatos = 250000;
+    let descuento = precioZapatos * 0.5;
+    let precioTotal = (precioZapatos - descuento) * totalCantidadNumeroInt;
+    if (cantidadNumeroInt == 50) {
+        document.getElementById("idcantidadNumeroProducto").innerHTML = 50;
+        precioTotal = (precioZapatos - descuento) * 50;
+    }
+    idPrecioTotal.innerHTML = "$" + precioTotal.toLocaleString('mx') ;
+
+})
