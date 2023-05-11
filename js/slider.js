898d292a3b55b684a870d5b73f4f9c8de2614702
Juanhoyos322 <juandiegohoyosrangel@gmail.com>
2023-05-11 11:26:18 -0500

    gl

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,21 @@
 let leftArrow = document.getElementById("leftArrow");
-let imgnoruega = document.getElementsBy("imgnoruega");
+let imgNoruegaCamry = document.getElementsBy("imgNoruegaCamry");
+let imgNoruegaPlaya = document.getElementById("imgNoruegaPlaya");
+let imgNoruegaRav4 = document.getElementById("imgNoruegaRav4");
 
 leftArrow.addEventListener("click", function(){
-    leftArrow.classList.remove("img-noruega");
+    if (imgNoruegaCamry.classlist.contains("img-Noruega-Camry")){
+        imgNoruegaCamry.classlist.remove("img-Noruega-Camry");
+        imgNoruegaCamry.classlist.add("img-Noruega-Camry-hidden");
+        imgNoruegaPlaya.classlist.add("img-Noruega-Playa");
+        imgNoruegaPlaya.classList.remove("img-Noruega-Playa-hidden-right");
+    }
+    else if(imgNoruegaPlaya.classList.contains("img-Noruega-Playa")) {
+        imgNoruegaPlaya.classList.remove("img-Noruega-Playa");
+        imgNoruegaPlaya.classList.add("img-Noruega-Playa-hidden-left");
+        imgNoruegaRav4.classList.add("img-Noruega-rav4");
+        imgNoruegaRav4.classList.remove("img-Noruega-rav4-hidden-right");
+    }
+
     
 });
