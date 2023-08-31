fdec2cf1d29ec8240a1093c80dbc91aa2bc84af2
Juank <juanrealpe972@gmail.com>
2023-08-31 07:55:53 -0500

    modi

--- js/homecenter.js
+++ js/homecenter.js
@@ -1,20 +1,103 @@
-let lupamovil = document.getElementById("lupamovil");
-let buscadorcell = document.getElementById("buscadorcell");
-let xSalid = document.getElementById("xSalid");
-let buscador = document.querySelector(".buscador");
-let tiempoDemora = 10000;
 
-lupamovil.addEventListener("click", function() {
-    buscadorcell.classList.add("seekerCell-hidden");
 
-    buscadorcell.classList.remove("buscador-movil");
-    
-    setTimeout(function() {
-        buscador.classList.toggle("expandido");
-    }, tiempoDemora);
+//form ANA//
+
+let iconArrowLeft = document.getElementById("iconArrowLeft");
+let iconArrowRight = document.getElementById("iconArrowRight")
+let idSlider = document.getElementById("idSlider");
+let boxBtn1 = document.getElementById("boxBtn1");
+let boxBtn2 = document.getElementById("boxBtn2");
+let boxBtn3 = document.getElementById("boxBtn3");
+let boxBtn4 = document.getElementById("boxBtn4");
+
+
+
+
+iconArrowLeft.addEventListener("click", function(){
+    if(idSlider.classList.contains("slider-view-2")) {
+        idSlider.classList.add("slider-view-1");
+        idSlider.classList.remove("slider-view-2");
+       
+    }
+    else if (idSlider.classList.contains("slider-view-3")) {
+        idSlider.classList.add("slider-view-2");
+        idSlider.classList.remove("slider-view-3");
+    }
+    else if (idSlider.classList.contains("slider-view-4")) {
+        idSlider.classList.add("slider-view-3");
+        idSlider.classList.remove("slider-view-4");
+    }
 });
 
-xSalid.addEventListener("click", function() {
-    buscadorcell.classList.remove("seekerCell-hidden");
-    buscadorcell.classList.add("buscador-movil");
+iconArrowRight.addEventListener("click", function(){
+    if(idSlider.classList.contains("slider-view-1")) {
+        idSlider.classList.add("slider-view-2");
+        idSlider.classList.remove("slider-view-1");
+        boxBtn1.classList.remove("btn-select");
+        boxBtn2.classList.add("btn-select");
+    }
+    else if (idSlider.classList.contains("slider-view-2")) {
+        idSlider.classList.add("slider-view-3");
+        idSlider.classList.remove("slider-view-2");
+    }
+    else if (idSlider.classList.contains("slider-view-3")) {
+        idSlider.classList.add("slider-view-4");
+        idSlider.classList.remove("slider-view-3");
+    }
 });
+
+
+let x = document.getElementById("x");
+let anajs = document.getElementById("anajs");
+let icon = document.getElementById("icon")
+
+
+
+x.addEventListener("click",function(){
+    if (anajs.style.display = "none") {
+         anajs.style.display === "grid"
+         x.style.display = "none"
+         icon.style.display = "flex"
+    } else{
+        anajs.style.display = "none"
+        icon.style.display ="flex";
+    } 
+}
+
+);
+
+icon.addEventListener("click",function(){
+    if (anajs.style.display = "none") {
+         icon.style.display = "none"
+         anajs.style.display = "grid"
+         x.style.display = "grid"
+    } else{
+        anajs.style.display = "none";
+        
+    } 
+}
+
+);
+
+
+// icon.addEventListener("click", function(){
+//     if (anajs.style.display === "none"){
+//         anajs.anajs.display = "grid"
+//         icon.style.display === "none"
+//     } else {
+
+//     }
+// });
+
+// icon.addEventListener("click",function(){
+//     if (anajs.style.display === "none") {
+//          anajs.style.display = "grid"
+//          x.style.display ==="none"
+//          x.style.display = "grid"
+//     } else{
+//         anajs.style.display = "none"
+//         x.style.display = "none"; 
+//     } 
+// });
+
+
